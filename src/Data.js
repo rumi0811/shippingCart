b57ec3cfc9bcfb217b199e215722fc45b8e15e6c
rumi0811 <sekarpuspitaarum08@gmail.com>
2023-04-15 22:10:05 +0700

    little more

---
+++ src/Data.js
@@ -0,0 +1,29 @@
+let shopItemsData = [{
+    id:"aswd1",
+    name:"Casual Shirt",
+    price: 45,
+    desc: "lorem73  ",
+    img: "images/img-1.jpg",
+}, 
+{
+    id:"aswd2",
+    name:"Office Shirt",
+    price: 100,
+    desc: "lorem77  ",
+    img: "images/img-2.jpg",   
+}, 
+{
+    id:"aswd3",
+    name:"T Shirt",
+    price: 25,
+    desc: "lorem79  ",
+    img: "images/img-3.jpg",
+}, 
+{
+    id:"aswd4",
+    name:"Man Shirt",
+    price: 300,
+    desc: "lorem70  ",
+    img: "images/img-4.jpg",
+},
+];
--- src/main.js
+++ src/main.js
@@ -1,39 +1,8 @@
 let shop = document.getElementById('shop')
 
-let shopItemsData = [{
-    id:"aswd1",
-    name:"Casual Shirt",
-    price: 45,
-    desc: "lorem73  ",
-    img: "images/img-1.jpg",
-}, 
-{
-    id:"aswd2",
-    name:"Office Shirt",
-    price: 100,
-    desc: "lorem77  ",
-    img: "images/img-2.jpg",   
-}, 
-{
-    id:"aswd3",
-    name:"T Shirt",
-    price: 25,
-    desc: "lorem79  ",
-    img: "images/img-3.jpg",
-}, 
-{
-    id:"aswd4",
-    name:"Man Shirt",
-    price: 300,
-    desc: "lorem70  ",
-    img: "images/img-4.jpg",
-},
-];
+
 
 let basket = JSON.parse(localStorage.getItem("data")) || [];
-
-
-
 
 let generateShop =()=>{
     return (shop.innerHTML = shopItemsData
